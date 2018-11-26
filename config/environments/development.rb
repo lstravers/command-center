d0a4219af56c5674461e830bd0352fc243cd0adc
lstravers <stravers222@gmail.com>
2018-11-25 21:50:28 -0500

    uncommented bycrypt and add devise to gemfile.lock; add background image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # In the development environment your application's code is reloaded on every request. This slows down response time but is perfect for development since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -29,6 +27,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+
+  # Default url mailer options. In production :host should be set to the actual host of the app.
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
