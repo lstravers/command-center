23b4d54b828117b22ad961b443831ab802bcdfaf
lstravers <stravers222@gmail.com>
2018-12-13 22:29:37 -0500

    modify welcome page text and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user
+  skip_before_action :authenticate_user! , only: [:create]
+  before_action :authenticate_user!
+
+  # def new
+  #   @user = User.new
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :encryptable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :encryptable,
+         :registerable, :recoverable, :rememberable, :validatable
+         
   has_secure_token :auth_token
   has_secure_password
 
